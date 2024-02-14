98eed825d9fe9e815a9dba5d1afb30550ce7ef3d
Shivam1tripathi <126380884+Shivam1tripathi@users.noreply.github.com>
2024-02-14 11:05:50 +0000

    book my show app

--- src/components/Entertainment/EntertainmentCard.Component.jsx
+++ src/components/Entertainment/EntertainmentCard.Component.jsx
@@ -17,20 +17,19 @@ const EntertainmentCard = (props) => {
 
 const EntertainmentCardSlider = () => {
   const EntertainmentImage = [
-    "https://in.bmscdn.com/discovery-catalog/collections/tr:w-800,h-800:ote-MTI1KyBFdmVudHM%3D,otc-FFFFFF,otf-Roboto,ots-64,ox-48,oy-320,ott-b:w-300/workshops-collection-202007231330.png",
-    "https://in.bmscdn.com/discovery-catalog/collections/tr:w-800,h-800:ote-OSBFdmVudHM%3D,otc-FFFFFF,otf-Roboto,ots-64,ox-48,oy-320,ott-b:w-300/fitness-collection-2020081150.png",
-    "https://in.bmscdn.com/discovery-catalog/collections/tr:w-800,h-800:ote-MjArIEV2ZW50cw%3D%3D,otc-FFFFFF,otf-Roboto,ots-64,ox-48,oy-320,ott-b:w-300/kids-collection-202007220710.png",
-    "https://in.bmscdn.com/discovery-catalog/collections/tr:w-800,h-800:ote-NDArIEV2ZW50cw%3D%3D,otc-FFFFFF,otf-Roboto,ots-64,ox-48,oy-320,ott-b:w-300/comedy-shows-collection-202007220710.png",
-    "https://in.bmscdn.com/discovery-catalog/collections/tr:w-800,h-800:ote-NSBFdmVudHM%3D,otc-FFFFFF,otf-Roboto,ots-64,ox-48,oy-320,ott-b:w-300/music-shows-collection-202007220710.png",
-    "https://in.bmscdn.com/discovery-catalog/collections/tr:w-800,h-800:ote-MTUrIEV2ZW50cw%3D%3D,otc-FFFFFF,otf-Roboto,ots-64,ox-48,oy-320,ott-b:w-300/esports-collection-202011150107.png",
-    "https://in.bmscdn.com/discovery-catalog/collections/tr:w-800,h-800:ote-OSBFdmVudHM%3D,otc-FFFFFF,otf-Roboto,ots-64,ox-48,oy-320,ott-b:w-300/self-improvement-collection-202007220710.png",
-    "https://in.bmscdn.com/discovery-catalog/collections/tr:w-800,h-800:w-300/cooking-collection-202007222211.png",
-    "https://in.bmscdn.com/discovery-catalog/collections/tr:w-800,h-800:ote-MSBFdmVudA%3D%3D,otc-FFFFFF,otf-Roboto,ots-64,ox-48,oy-320,ott-b:w-300/interactive-games-collection-202012041129.png",
-    "https://in.bmscdn.com/discovery-catalog/collections/tr:w-800,h-800:ote-MTArIEV2ZW50cw%3D%3D,otc-FFFFFF,otf-Roboto,ots-64,ox-48,oy-320,ott-b:w-300/art-and-crafts-collection-202007220710.png",
-    "https://in.bmscdn.com/discovery-catalog/collections/tr:w-800,h-800:ote-NCBFdmVudHM%3D,otc-FFFFFF,otf-Roboto,ots-64,ox-48,oy-320,ott-b:w-300/theatre-shows-collection-202012041128.png",
-    "https://in.bmscdn.com/discovery-catalog/collections/tr:w-800,h-800:w-300/adventure-collection-202010140844.png",
+    "https://i.pinimg.com/474x/58/96/38/589638ea299c5277710dedeeb1210486.jpg",
+    "https://th.bing.com/th/id/OIP.3JrpyZy_d2bq9XuuP7Y33wHaKs?rs=1&pid=ImgDetMain",
+    "https://m.media-amazon.com/images/M/MV5BNGYyZGM5MGMtYTY2Ni00M2Y1LWIzNjQtYWUzM2VlNGVhMDNhXkEyXkFqcGdeQXVyMTMxODk2OTU@._V1_UY209_CR0,0,140,209_AL_.jpg",
+    "https://stat2.bollywoodhungama.in/wp-content/uploads/2016/03/74637389.jpg",
+    "https://toptenfamous.com/wp-content/uploads/2022/03/MV5BMGE3Yjg3NzItNjlmOS00YjY2LThiMDctMzM0NDA2NmExYTg4XkEyXkFqcGdeQXVyMTQ3ODQ3NTcz._V1_FMjpg_UX1000_-768x1112.jpg",
+    "https://th.bing.com/th/id/OIP.Rgg8p8ioWy7MGHWaHTDd9wHaJ8?w=487&h=654&rs=1&pid=ImgDetMain",
+    "https://th.bing.com/th/id/OIP.UrPRDvX-Zh3uPO9BesqFowHaKx?w=2000&h=2910&rs=1&pid=ImgDetMain",
+    "https://th.bing.com/th/id/OIP.f6wSsTECv5UF-uPCgMWk_wHaKu?w=1105&h=1600&rs=1&pid=ImgDetMain",
+    "https://th.bing.com/th/id/OIP.ek8WuNCUxkZQBtngD2a8MQHaJQ?w=640&h=800&rs=1&pid=ImgDetMain",
+    "https://th.bing.com/th/id/OIP.WqvWiwoyMVA4UfZ7eoBgeQAAAA?w=474&h=663&rs=1&pid=ImgDetMain",
+    "https://th.bing.com/th/id/OIP.eUIwffhN5R_cyGqWJwgQ9QAAAA?rs=1&pid=ImgDetMain",
+    "https://i1.wp.com/www.socialnews.xyz/wp-content/uploads/2019/04/16/Salman-Khan-s-Bharat-movie-latest-poster-.jpg?fit=650%2C866&quality=90&zoom=1&ssl=1",
   ];
-
   const settings = {
     infinite: false,
     autoplay: false,
--- src/components/HeroCarousel/HeroCarousel.Component.jsx
+++ src/components/HeroCarousel/HeroCarousel.Component.jsx
@@ -38,6 +38,47 @@ const HeroCarousel = () => {
       vote_average: 7.4,
       vote_count: 346,
     },
+    {
+      "adult": false,
+      "backdrop_path": "/90ez6ArvpO8bvpyIngBuwXOqJm5.jpg",
+      "genre_ids": [
+        35,
+        18,
+        10749
+      ],
+      "id": 19404,
+      "original_language": "hi",
+      "original_title": "दिलवाले दुल्हनिया ले जायेंगे",
+      "overview": "Raj is a rich, carefree, happy-go-lucky second generation NRI. Simran is the daughter of Chaudhary Baldev Singh, who in spite of being an NRI is very strict about adherence to Indian values. Simran has left for India to be married to her childhood fiancé. Raj leaves for India with a mission at his hands, to claim his lady love under the noses of her whole family. Thus begins a saga.",
+      "popularity": 27.617,
+      "poster_path": "/lfRkUr7DYdHldAqi3PwdQGBRBPM.jpg",
+      "release_date": "1995-10-20",
+      "title": "Dilwale Dulhania Le Jayenge",
+      "video": false,
+      "vote_average": 8.539,
+      "vote_count": 4333
+    },
+    {
+      "adult": false,
+      "backdrop_path": "/nMKdUUepR0i5zn0y1T4CsSB5chy.jpg",
+      "genre_ids": [
+        18,
+        28,
+        80,
+        53
+      ],
+      "id": 155,
+      "original_language": "en",
+      "original_title": "The Dark Knight",
+      "overview": "Batman raises the stakes in his war on crime. With the help of Lt. Jim Gordon and District Attorney Harvey Dent, Batman sets out to dismantle the remaining criminal organizations that plague the streets. The partnership proves to be effective, but they soon find themselves prey to a reign of chaos unleashed by a rising criminal mastermind known to the terrified citizens of Gotham as the Joker.",
+      "popularity": 95.582,
+      "poster_path": "/qJ2tW6WMUDux911r6m7haRef0WH.jpg",
+      "release_date": "2008-07-16",
+      "title": "The Dark Knight",
+      "video": false,
+      "vote_average": 8.515,
+      "vote_count": 31431
+    }
   ]);
 
   const settingsLG = {
